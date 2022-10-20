8e39c5f79299a5e4787084c4d9f3596141af90ea
Ahmad Al Khateeb <al.khateeb.ahmad96@gmail.com>
2022-10-20 20:50:53 +0200

    Fixes the login.
    Now you can register a new user, login and out.

--- ProjektApp/Controllers/AuctionsController.cs
+++ ProjektApp/Controllers/AuctionsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjektApp.Core;
 using ProjektApp.Core.Interfaces;
@@ -6,6 +7,7 @@ using ProjektApp.ViewModels;
 
 namespace ProjektApp.Controllers
 {
+    [Authorize]
     public class AuctionsController : Controller
     {
         private readonly IAuctionService _auctionService;
